3e3e3d7d76b9657052b5247770b5ab0af37e03a5
onionchop <ruthhyo@gmail.com>
2022-08-15 11:14:08 -0400

    added stylesheet and script to html

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// timer 
+
+// count variable
+
+// need a buttion and event listener
+
+// var questions = [
+//  {
+//        question: "What does API stand for?"
+//        possibleAnswers: ["Application Programming Interface", "Access P I", "Agent Performance Indicators"]
+//        correctAnswer: "Application Programming Interface"
+// }  
+// ]
+
+// var questionCounter = 0
+// var timeCounter = 100
+
+// Start Button 
+// function startGame(){
+//set attr of welcome div to hude
+//set attr of questions to visible
+//start loop thrpugh ques
+//update h1 textContent with ques[questionCounter].question
+//loop through and create possible answer buttons questions[questionCounter].possibleAnswers
+// add listener to possibleAnswers button
+//  possibleAnswersBtn.addEventListener('click', function (event){
+//      var selectedAnswer = event.target
+//     get value of target
+//   compare value to questions[questionCounter].correctAnswer
+// if(questions[questionCounter].correctAnswer !== selectedAnswer.value){
+// deduct 10 from timerCounter - 10 and questionCounter++
+//  return startGame() or askQuestions()
+//}
+//}
